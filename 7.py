1408b104c160a688ddb55b8083c871c4921cd71a
Artemka08 <Artyom08112006@gmail.com>
2023-03-30 11:18:29 +0300

    added solution for 7th task

--- 7.py
+++ 7.py
@@ -2,24 +2,27 @@ def replace_f(string, index):
     _lst = list(string)
     _lst[index] = "_"
     string = "".join(_lst)
-    # string[index] = "_"
     return string
 
-def find_f(string, first_index="No", last_index="No"):
-    f_index = string.find("f")
-    if f_index == -1 and first_index == "No":
-        return
-    elif f_index != -1 and first_index == "No":
-        first_index = f_index
-        string = replace_f(string, first_index)
-        print(string)
-        find_f(string, first_index)
-    elif f_index != -1 and first_index != "No":
-        last_index = f_index
-        string = replace_f(string, last_index)
-        print(string)
-        find_f(string, first_index, last_index)
+def find_last_f(string, last_index="NO"):
+    if string.find("f") == -1:
+        return last_index
     else:
-        return f" Hi {first_index}, {last_index}"
+        last_index = string.find("f")
+        string = replace_f(string, last_index)
+        return find_last_f(string, last_index=last_index)
 
-print(find_f("office"))
+def find_f(string):
+    first_index = string.find("f")
+    string = replace_f(string, first_index)
+    last_index = find_last_f(string)
+    if first_index == -1:
+        return
+    elif last_index == "NO":
+        return first_index
+    else:
+        return f"{first_index} {last_index}"
+
+
+
+print(find_f("I want a high five for this homework."))
